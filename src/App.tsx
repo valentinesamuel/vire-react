da8952c394d9f526faa3a35132b362e83adfc7f7
valentinesamuel <valentinesamuel2580@gmail.com>
2023-01-29 02:33:00 +0100

    Trying Linter

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { Person } from './/components/Person';
-import { useState } from "react";
-
+import { useState } from 'react';
 
 import reactLogo from './assets/react.svg';
 import './App.css';
@@ -9,15 +8,10 @@ function App() {
   const names = ['John', 'Jane', 'Michael', 'Emily'];
   const [count, setCount] = useState(0);
 
-
-
-
-
-
   return (
     <div className="App">
       <div>
-          <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
         <a href="https://reactjs.org" target="_blank">
