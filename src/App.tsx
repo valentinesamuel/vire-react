2f202e46270b30b27206a31ddf4d85813753c540
valentinesamuel <valentinesamuel2580@gmail.com>
2023-01-29 02:17:48 +0100

    Trying to add script

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { Person } from './/components/Person';
-import { useState } from 'react';
+import { useState } from "react";
+
+
 import reactLogo from './assets/react.svg';
 import './App.css';
 
@@ -7,10 +9,15 @@ function App() {
   const names = ['John', 'Jane', 'Michael', 'Emily'];
   const [count, setCount] = useState(0);
 
+
+
+
+
+
   return (
     <div className="App">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+          <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
         <a href="https://reactjs.org" target="_blank">
